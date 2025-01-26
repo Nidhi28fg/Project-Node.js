c0d221d2e520514305af0dcca5b67bae9d79b329
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2025-01-26 14:59:57 +0530

    Update index.js

--- 1st project/Fetch API and the async/index.js
+++ 1st project/Fetch API and the async/index.js
@@ -1,1 +1,24 @@
+// Using Fetch API with async/await
+async function main() {
+    try {
+        const response = await fetch("https://sum-server.100xdevs.com/todos");
+        const json = await response.json();
+        console.log(json.todos.length);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
 
+main();
+
+// Fetch API:
+
+// The first part (commented out) uses the Fetch API to request data from a URL.
+// It waits for the response and processes it using .then(), an older way of handling promises.
+
+
+//Async/Await:
+
+//The second part defines an async function called main.
+// It uses await to handle the Fetch call, simplifying the syntax and making it easier to read.
+// It fetches data, then converts the response to JSON and logs the length of the  todos array
